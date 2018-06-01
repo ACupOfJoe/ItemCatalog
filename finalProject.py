ced3bf4e0f2f5f78c387b452d64a8101c20a8c18
vagrant <vagrant@vagrant.vm>
2018-06-01 23:47:10 +0000

    Finish first draft of createStockObject function

--- finalProject.py
+++ finalProject.py
@@ -15,13 +15,14 @@ app = Flask(__name__)
 
 
 #Fake Restaurants
-stock = {'symbol': '', 'id': '1', 'close_price': 0.00, 'data': ''}
-
+stockCatalogItem
 #https://eresearch.fidelity.com/eresearch/markets_sectors/sectors/sectors_in_market.jhtml
-StockCatalog = [{'name': 'Internet & Catalog Retail', 'id': '1'}, {'name':'Independent Power and Renewable Electricity Producers', 'id':'2'},{'name':'Real Estate Management & Development', 'id':'3'}, {'name':'Software', 'id':'3'}]
+stockCatalogAll = [{'name': 'Internet & Catalog Retail', 'id': '1'}, {'name':'Independent Power and Renewable Electricity Producers', 'id':'2'},{'name':'Real Estate Management & Development', 'id':'3'}, {'name':'Software', 'id':'3'}]
 
 
 #Fake Menu Items
+stock = {'symbol': '', 'id': '1', 'close_price': 0.00, 'data': ''}
+listOfStocks = [stock]
 items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1', 'restaurant_id': '1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2', 'restaurant_id':'1'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3', 'restaurant_id':'2'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4', 'restaurant_id':'3'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5', 'restaurant_id':'1'} ]
 item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree', 'id': '1', 'restaurant_id': '1'}
 
@@ -37,3 +38,18 @@ stock['id'] = '1'
 time = time.strftime("%Y/%m/%d")
 stock['close_price'] = (df1.loc[df1['Date'] == (pd.to_datetime(time))]['Close'].reset_index())
 print stock['close_price'] = stock['close_price']['Close']
+
+def createStockObject(ticker): 
+	startTime = datetime.date.today()-datetime.timedelta(days=365)
+	df1 = web.DataReader(ticker, 'morningstar',
+	            startTime,
+	            datetime.date.today()).reset_index()
+	stock['symbol'] = df1.loc[df1['Date'] == (pd.to_datetime(time))]['Symbol'].reset_index()
+	stock['symbol'] = stock['symbol']['Symbol']
+	stock['id'] = str(len(listOfStocks))
+	stock['close_price'] = (df1.loc[df1['Date'] == (pd.to_datetime(time))]['Close'].reset_index())
+	stock['close_price'] = stock['close_price']['Close']
+	stock['data'] = df1['Close']
+	return stock.copy()
+
+
