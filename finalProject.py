33978558bbb4cbe254be22749410ced364f232b2
vagrant <vagrant@vagrant.vm>
2018-06-10 21:07:54 +0000

    Finish createStock method. Use createStockObject method

--- finalProject.py
+++ finalProject.py
@@ -2,6 +2,15 @@ from flask import Flask, render_template, request, url_for, redirect, jsonify, f
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker 
 from database_setup import User, Stock, Industry, Base 
+
+from database_setup import Base, Industry, Stock, User
+import datetime
+from datetime import timedelta
+import numpy as np
+import pandas as pd 
+pd.core.common.is_list_like = pd.api.types.is_list_like
+import pandas_datareader.data as web
+
 app = Flask(__name__)
 
 def openSession(): 
@@ -21,7 +30,7 @@ def readIndustries():
 	session.close()
 	return render_template('readIndustries.html', industries=industries)
 
-@app.route('/industries/new/', methods=['GET', 'POST'])
+@app.route('/industries/create/', methods=['GET', 'POST'])
 def createIndustry(): 
 	session = openSession()
 	if request.method == "POST":
@@ -71,12 +80,45 @@ def deleteIndustry(industry_id):
 	else:
 		return render_template('deleteIndustry.html', industry=deleteThisIndustry)
 
-@app.route('/industries/<int:industry_id>/')
+@app.route('/industries/<int:industry_id>/', methods=['GET', 'POST'] )
 def readStocks(industry_id): 
 	session = openSession()
 	industry = session.query(Industry).filter_by(id=industry_id).one()
 	stocks=session.query(Stock).filter_by(industry_id=industry_id)
-	return render_template('readStocks.html', industry=industry, stocks=stocks)
+	return render_template('readStocks.html', industry=industry, stocks=stocks, industry_id=industry_id)
+
+@app.route('/industries/<int:industry_id>/stocks/create', methods=['GET', 'POST'])
+def createStock(industry_id):
+	#try: 
+	session = openSession()
+	if request.method == "POST":
+		print "Method is post"
+		newStock = createStockObject(ticker=str(request.form['ticker']), industry_id=industry_id)
+		session.add(newStock)
+		flash('Stock Successfully Added {0}'.format(newStock.ticker))
+		session.commit()
+		session.close()
+		return redirect(url_for('readStocks', industry_id=industry_id))
+	#except:
+		#return "Error. Possibly your Stock Ticker doesn't exist"
+	else: 
+		return render_template('createStock.html', industry_id=industry_id)
+
+def createStockObject(ticker, industry_id): 
+	startTime = datetime.date.today()-datetime.timedelta(days=4)
+	print "Readin Data"
+	df1 = web.DataReader(ticker, 'morningstar', 
+	           	startTime,
+	            datetime.date.today(), retry_count=0).reset_index()
+	print "Read Data"
+	today= datetime.datetime.today()
+	yesterday = today - timedelta(days=2)
+	yesterday = yesterday.strftime("%Y-%m-%d")
+	close_price = df1['Close'].tail(1)
+	close_price = str(close_price.to_string(index=False))
+	print "Set up variables"
+	print close_price[0:]
+	return Stock(ticker=ticker, close_price=close_price, industry_id=industry_id)
 
 @app.route('/HelloWorld')
 def main(): 
