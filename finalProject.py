a3d7fe817b3c9097f818b7e5b1daaf8272a3d3d2
vagrant <vagrant@vagrant.vm>
2018-06-11 00:08:34 +0000

    Finish updateStock method

--- finalProject.py
+++ finalProject.py
@@ -81,6 +81,7 @@ def deleteIndustry(industry_id):
 		return render_template('deleteIndustry.html', industry=deleteThisIndustry)
 
 @app.route('/industries/<int:industry_id>/', methods=['GET', 'POST'] )
+@app.route('/industries/<int:industry_id>/stocks', methods=['GET', 'POST'] )
 def readStocks(industry_id): 
 	session = openSession()
 	industry = session.query(Industry).filter_by(id=industry_id).one()
@@ -105,20 +106,45 @@ def createStock(industry_id):
 		return render_template('createStock.html', industry_id=industry_id)
 
 def createStockObject(ticker, industry_id): 
-	startTime = datetime.date.today()-datetime.timedelta(days=4)
-	print "Readin Data"
-	df1 = web.DataReader(ticker, 'morningstar', 
-	           	startTime,
-	            datetime.date.today(), retry_count=0).reset_index()
-	print "Read Data"
-	today= datetime.datetime.today()
-	yesterday = today - timedelta(days=2)
-	yesterday = yesterday.strftime("%Y-%m-%d")
-	close_price = df1['Close'].tail(1)
-	close_price = str(close_price.to_string(index=False))
-	print "Set up variables"
-	print close_price[0:]
-	return Stock(ticker=ticker, close_price=close_price, industry_id=industry_id)
+	try: 
+		startTime = datetime.date.today()-datetime.timedelta(days=4)
+		print "Readin Data"
+		df1 = web.DataReader(ticker, 'morningstar', 
+		           	startTime,
+		            datetime.date.today(), retry_count=0).reset_index()
+		print "Read Data"
+		today= datetime.datetime.today()
+		yesterday = today - timedelta(days=2)
+		yesterday = yesterday.strftime("%Y-%m-%d")
+		close_price = df1['Close'].tail(1)
+		close_price = str(close_price.to_string(index=False))
+		print "Set up variables"
+		print close_price[0:]
+		return Stock(ticker=ticker, close_price=close_price, industry_id=industry_id)
+	except: 
+		print "Sorry, we could not find the ticker for that stock"
+
+@app.route('/industries/<int:industry_id>/stocks/<int:stock_id>/update/', methods=['GET', 'POST'])
+def updateStock(industry_id, stock_id):
+	session = openSession()
+	industry = session.query(Industry).filter_by(id=industry_id).one()
+	originalStock = session.query(Stock).filter_by(industry_id=industry_id, id=stock_id).one()
+	print "ticker and id are: " + str(originalStock.ticker) + str(originalStock.id)
+	if request.method == "POST":
+		print "Method is post"
+		if request.form['ticker']:
+			print "Request.form ticker exists"
+			updateStock = createStockObject(str(request.form['ticker']), industry_id=industry_id)
+			updateStock.id = originalStock.id
+			session.delete(originalStock)
+			print "New Id is : " + str(updateStock.id)
+			flash('Stock Successfully Edited {0}'.format(updateStock.ticker))
+			session.add(updateStock)
+			session.commit()
+			session.close()
+			return redirect(url_for('readStocks', industry_id=industry_id, industry=industry))
+	else:
+		return render_template('updateStock.html', stock=originalStock, stock_id=stock_id, industry=industry, industry_id=industry_id)
 
 @app.route('/HelloWorld')
 def main(): 
